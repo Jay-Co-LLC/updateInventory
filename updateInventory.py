20db7ccb61b8765c31da5c34ed6b9b0b44904958
Sam Rudd <samdrudd@gmail.com>
2019-09-26 00:22:38 -0700

    now use supplier sku to find product in ordoro

--- updateInventory.py
+++ updateInventory.py
@@ -9,12 +9,34 @@ headers = {}
 headers['Authorization'] = os.environ['ordoro_auth']
 headers['Content-Type'] = 'application/json'
 
+url = "https://api.ordoro.com"
+warehouse_id = os.environ["ordoro_warehouse_id"]
+
 def updateItems(items):
 	for each in items:
-		sku = each[0]
+		supplier_sku = each[0]
 		amt = each[1]
-		r = requests.put(f'https://api.ordoro.com/product/{sku}/warehouse/{os.environ["ordoro_warehouse_id"]}/', data=json.dumps({"on_hand" : amt}), headers=headers)
-		print(r.content)
+		params = {"search" : supplier_sku}
+		
+		# Get ordoro sku by searching via supplier_sku
+		r = requests.get(f'{url}/product/', params=params, headers=headers)
+		rob = r.json()
+		
+		if (rob['count'] == 0):
+			print(f"{rob['count']} found with Supplier SKU {supplier_sku}")
+			continue
+		
+		product = rob['product'][0]
+		ordoro_sku = product['sku']
+		
+		# Update the product in ordoro
+		r = requests.put(f'{url}/product/{ordoro_sku}/warehouse/{warehouse_id}/', data=json.dumps({"on_hand" : amt}), headers=headers)
+		rob = r.json()
+		
+		if (r.status_code == 200):
+			print(f"{rob['sku']} on-hand amount updated to {amt}.")
+		else:
+			print(f"{rob['error_message']}")
 		
 def main(event, context):
 	id = event['id']
